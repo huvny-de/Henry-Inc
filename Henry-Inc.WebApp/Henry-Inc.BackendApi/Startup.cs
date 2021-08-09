24f6f9da708d8613a345c4e9a8427f285b03fac1
Nhật Huy <henri.deaaron@gmail.com>
2021-08-09 08:30:44 +0700

    Add Swagger

--- Henry-Inc.WebApp/Henry-Inc.BackendApi/Startup.cs
+++ Henry-Inc.WebApp/Henry-Inc.BackendApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,7 +32,16 @@ namespace Henry_Inc.BackendApi
             services.AddControllersWithViews();
             services.AddDbContext<MyAppContext>(options =>
          options.UseSqlServer(Configuration.GetConnectionString(SystemConstant.MainConnectionString)));
+
             services.AddTransient<IPublicProductService, PublicProductService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Swagger Henry Inc",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +63,12 @@ namespace Henry_Inc.BackendApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Henry V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
