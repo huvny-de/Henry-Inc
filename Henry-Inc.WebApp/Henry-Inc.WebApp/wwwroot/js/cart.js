a9301bd9fb141f81a1842e208661c85ce1e55491
Hunvy <henri.deaaron@gmail.com>
2022-09-04 23:16:26 +0700

    better input shopping cart

--- Henry-Inc.WebApp/Henry-Inc.WebApp/wwwroot/js/cart.js
+++ Henry-Inc.WebApp/Henry-Inc.WebApp/wwwroot/js/cart.js
@@ -63,7 +63,7 @@
                     html += "<tr>"
                         + "<td> <img width=\"60\" src=\"" + $('#hidBaseAddress').val() + item.image + "\" alt=\"\" /></td>"
                         + "<td>" + item.description + "</td>"
-                        + "<td><div class=\"input-append\"><input class=\"span1\" style=\"max-width: 34px\" placeholder=\"1\" id=\"txt_quantity_" + item.productId + "\" value=\"" + item.quantity + "\" size=\"16\" type=\"text\">"
+                        + "<td><div class=\"input-append\"><input class=\"span1\" style=\"max-width: 34px;min-height: 47.33px !important;\" placeholder=\"1\" id=\"txt_quantity_" + item.productId + "\" value=\"" + item.quantity + "\" size=\"16\" type=\"text\">"
                         + "<button class=\"btn btn-minus\" data-id=\"" + item.productId + "\" type =\"button\"> <i class=\"icon-minus\"></i></button>"
                         + "<button class=\"btn btn-plus\" type=\"button\" data-id=\"" + item.productId + "\"><i class=\"icon-plus\"></i></button>"
                         + "<button class=\"btn btn-danger btn-remove\" type=\"button\" data-id=\"" + item.productId + "\"><i class=\"icon-remove icon-white\"></i></button>"
